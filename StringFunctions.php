c184577444063e621ab325618db75f05f9670e54
JMAR <529Design@users.noreply.github.com>
2017-08-22 22:39:07 -0400

    Create StringFunctions.php

---
+++ StringFunctions.php
@@ -0,0 +1,21 @@
+<html>
+  <p>
+    <?php
+    $myName = "Jeffrey";
+    print strlen($myName);
+    print substr($myName, 0, 4);
+    ?>
+  </p>
+  <p>
+    <?php
+    print strtoupper($myName);
+    
+    ?>
+  </p>
+  <p>
+    <?php
+    print strtolower($myName);
+    
+    ?>
+  </p>
+</html>
