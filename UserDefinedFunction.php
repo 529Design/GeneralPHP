37d77283d8fccc839bab65c60a6109ec53539d14
JMAR <529Design@users.noreply.github.com>
2017-08-24 14:57:38 -0400

    Create UserDefinedFunction.php

---
+++ UserDefinedFunction.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        
+        function greetings($name){
+         echo "Greetings, " . $name . "!";   
+        }
+        
+        $n = "Jeffrey";
+        greetings($n);
+        ?>
+      </p>
+    </body>
+</html>
